5e8cf89c93373f45cc816062f3167575f2ea933d
esmaelyassin58-cell <esmaelyassin58@gmail.com>
2026-06-26 02:24:41 +0400

    Update server.js

--- server.js
+++ server.js
@@ -12,3 +12,23 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 }); 
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🎉 Bingo Seven Pro Running with DB!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Server running on port " + PORT);
+});
