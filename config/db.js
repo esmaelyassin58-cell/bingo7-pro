cafe330a475737fe9c6b9af9b9ebe648c9be5396
esmaelyassin58-cell <esmaelyassin58@gmail.com>
2026-07-03 22:14:41 +0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("✅ MongoDB Connected");
+    // በ .env ፋይል ውስጥ ያለውን MONGO_URI ጥቅምና ካልተገኘ ደግሞ በ local እንዲሰራ ያደርጋል
+    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/bingo_db", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(💾 MongoDB Connected: ${conn.connection.host});
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error.message);
-    process.exit(1);
+    console.error(❌ Database Connection Error: ${error.message});
+    process.exit(1); // ስህተት ካለ ፕሮግራሙን ያቆመዋል
   }
 };
 
