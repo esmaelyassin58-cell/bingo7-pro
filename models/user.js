a2a4984d6386b984d7f927a9340634f89c677104
esmaelyassin58-cell <esmaelyassin58@gmail.com>
2026-07-03 22:12:49 +0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,14 @@
 
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  telegramId: { type: Number, required: true, unique: true },
+  firstName: { type: String },
+  phoneNumber: { type: String, required: true, unique: true },
+  walletBalance: { type: Number, default: 0 }, // የተጫዋቹ የኪስ ቦርሳ ብር
+  referredBy: { type: Number, default: null }, // የጋባዡ ሰው Telegram ID
+  isRegistered: { type: Boolean, default: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("User", UserSchema);
